aa4c35a4c32127e0c60370f720b1d521fcb7a42c
Alfa Arseniko <alfarseniko@gmail.com>
2025-04-01 14:32:58 +0200

    task: landing page complete

---
+++ src/firebase/index.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLwGPkuL-Z7QqbbmX3o-Z3fKbKY1BTV_4",
+  authDomain: "geo-bim-app.firebaseapp.com",
+  projectId: "geo-bim-app",
+  storageBucket: "geo-bim-app.firebasestorage.app",
+  messagingSenderId: "1095727267816",
+  appId: "1:1095727267816:web:1fefa0c5c282b2866f6e5d",
+  measurementId: "G-E4X84NXLVM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
--- src/react-components/MainPage.tsx
+++ src/react-components/MainPage.tsx
@@ -9,16 +9,24 @@ export function LandingPage() {
             <img src="/public/crewnix_white.svg"></img>
           </div>
           <div className="header-links">
-            <ul>About Us</ul>
-            <ul>Contact Us</ul>
-            <ul>Login</ul>
+            <ul>
+              <a href="https://www.linkedin.com/in/bhatti25/" target="_blank">
+                Contact Us
+              </a>
+            </ul>
+            <ul></ul>
+            <button id="login-button">LOG IN</button>
           </div>
         </header>
         <div id="landing-items">
           <h1>GeoBIM</h1>
           <h2>Your BIM Model on the Globe</h2>
+          <button className="cta-button">Get started</button>
         </div>
-        <footer></footer>
+        <footer>
+          Made with 🧠 for the 🌍 by CREWNIX
+          <img src="/public/crewnix_white.svg"></img>{" "}
+        </footer>
       </div>
     </>
   );
